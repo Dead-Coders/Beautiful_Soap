3ee82663e881afbf41db210ca0640d91f2c9a3b1
Abdullah <itsabdullah14311211@gmail.com>
2024-08-30 11:13:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from bs4 import BeautifulSoup
+url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+r = requests.get(url)
+
+
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
+for heading in soup.find_all("h2"):
+  print(heading.text)
